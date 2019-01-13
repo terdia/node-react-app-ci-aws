e0c6263d4d6ffad50c783229478e5b3a2af943b0
Osayawe Ogbemudia Terry <terrymarcy2000@yahoo.com>
2019-01-13 21:25:14 +0100

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -9,16 +9,9 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi there. lets play the game
+            Simple react app, with continues integration (Travis CI)
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        
         </header>
       </div>
     );
